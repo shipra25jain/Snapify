3bdde732f5e0a6ed23fca5f6099935aebf30a9ef
Glitch (buttercup-visage) <none>
2018-11-24 10:27:51 +0000

    🐌🛬 Checkpoint ./server.py:2366103/472:2366152/21 ./views/index.html:2366103/73:2366152/1645 ./requirements.txt:2366103/8

--- server.py
+++ server.py
@@ -2,25 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
 
 # Spotify API wrapper, documentation here: http://spotipy.readthedocs.io/en/latest/
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from werkzeug.utils import secure_filename
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # Authenticate with Spotify using the Client Credentials flow
 client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 app = Flask(__name__, static_folder='public', template_folder='views')
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def homepage():
     # Displays homepage
     return render_template('index.html')
   
 @app.route('/upload_image', methods=['GET'])
-def upload_image(): #this is old 
+def upload_image(): #this is old 'def new_releases()' 
   
     # Use the country from the query parameters, if provided
     if 'country' in request.args:
