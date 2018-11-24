efdffa650b4505ca9282a836053458d8d4161213
Glitch (buttercup-visage) <none>
2018-11-24 11:31:40 +0000

    🍣📒 Checkpoint ./views/index.html:2366103/335 ./server.py:2366103/2872

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from flask import Flask, request, render_template, jsonify, redirect, url_for
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = '/path/to/the/uploads'
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 # Authenticate with Spotify using the Client Credentials flow
@@ -16,43 +16,55 @@ client_credentials_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 app = Flask(__name__, static_folder='public', template_folder='views')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/')
 def homepage():
     # Displays homepage
     return render_template('index.html')
   
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
+@app.route("/upload", methods=["POST"])
+def upload():
+    folder_name = request.form['superhero']
     '''
+    # this is to verify that folder to upload to exists.
+    if os.path.isdir(os.path.join(APP_ROOT, 'files/{}'.format(folder_name))):
+        print("folder exist")
+    '''
+    target = os.path.join(APP_ROOT, 'files/{}'.format(folder_name))
+    print(target)
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    print(request.files.getlist("file"))
+    for upload in request.files.getlist("file"):
+        print(upload)
+        print("{} is the file name".format(upload.filename))
+        filename = upload.filename
+        # This is to verify files are supported
+        ext = os.path.splitext(filename)[1]
+        if (ext == ".jpg") or (ext == ".png"):
+            print("File supported moving on...")
+        else:
+            render_template("Error.html", message="Files uploaded are not supported...")
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
+
+    # return send_from_directory("images", filename, as_attachment=True)
+    return render_template("complete.html", image_name=filename)
+
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+
+@app.route('/gallery')
+def get_gallery():
+    image_names = os.listdir('./images')
+    print(image_names)
+    return render_template("gallery.html", image_names=image_names)
 # @app.route('/upload_image', methods=['GET'])
 # def upload_image(): #this is old 'def new_releases()' 
   
