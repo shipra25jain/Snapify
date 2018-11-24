7992f8f5e955710367e36194712c6701e974455d
Glitch (buttercup-visage) <none>
2018-11-24 13:42:14 +0000

    ☠️🐊 Checkpoint ./server.py:2353708/22

--- server.py
+++ server.py
@@ -33,6 +33,7 @@ def homepage():
   
 @app.route("/upload", methods=['POST'])
 def upload():
+    print("hi")
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
