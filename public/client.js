423367ff94699e4faac1ec76e7f4e4c1714ccc32
Glitch (buttercup-visage) <none>
2018-11-24 19:35:02 +0000

    🚒🗃 Checkpoint ./public/client.js:2366117/2670:2366143/6

--- public/client.js
+++ public/client.js
@@ -28,3 +28,33 @@ $(function() {
   });
 
 });
+
+// Get the hash of the url
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = 'a07069568b97405a9df92518444b5245';
+const redirectUri = 'https://buttercup-visage.glitch.me/';
+const scopes = [
+  'user-read'
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+}
