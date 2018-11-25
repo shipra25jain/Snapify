b33026bbdb09ab39943f931491ad15b817b1795c
Glitch (buttercup-visage) <none>
2018-11-25 11:29:24 +0000

    🗾🚞 Checkpoint ./views/index.html:2366117/212 ./public/client.js:2375538/6

--- public/client.js
+++ public/client.js
@@ -108,9 +108,10 @@ document.getElementById('file-picker').addEventListener('change', function(event
   console.log('before');
   setTimeout(function(){
     console.log('after');
-  },500);
+  },1000);
   var newImg = "/app/images/";
   console.log(newImg.concat(image.name));
+  
   document.getElementById("main_img").src = newImg.concat(image.name);
   // readTextFile('/app/public/imagedataFile.txt')
 });
